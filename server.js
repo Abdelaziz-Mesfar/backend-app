971fa03cbfb0dd07acdfb966d920b72d70d0ac9a
Abdelaziz Mesfar <abdelaziz.mesfar01@gmail.com>
2022-08-21 10:58:07 +0100

    fix cors policy issue

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.options('*', cors());
 
 app.use('/patients', patientRouter);
 app.use('/appointments', AppointmentRouter);
