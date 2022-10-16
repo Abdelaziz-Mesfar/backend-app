eb26fad6ba80cbe4a84e28bd0b86e56119b0e768
Abdelaziz Mesfar <abdelaziz.mesfar01@gmail.com>
2022-10-16 18:50:31 +0100

    add email verification

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
+const Token = require('../models/tokenModel');
+const sendEmail = require('../config/nodemailer');
+const crypto = require('crypto');
 const { registerValidator, loginValidator } = require('../utilities/validators')
 
 const registerUser = async (req, res) => {
@@ -25,12 +28,25 @@ const registerUser = async (req, res) => {
             password: hashedPassword
         })
         const createdUser = await user.save()
+
+        const token = await new Token({
+            userId: createdUser._id,
+            token: crypto.randomBytes(32).toString("hex")
+        }).save();
+        
+        const html = `<h1> Welcome to My Clinic </h1>
+                        <p>You can verify your account simply by <a href='${process.env.BASE_URL}/${createdUser._id}/verify/${token.token}'>Clicking here</a></p>`
+        // const url = `${process.env.BASE_URL}/${createdUser._id}/verify/${token.token}`
+
+        await sendEmail(createdUser.email, "Account Confirmation", html)
+
         res.status(201).json({
-            message: 'account created successfully',
+            message: 'An email was sent to your account please verify',
             user: createdUser
         })
     } catch (error) {
         res.status(500).json({ error: error.message })
+        console.log(error);
     }
 }
 
@@ -54,6 +70,11 @@ const loginUser = async (req, res) => {
                     error: 'wrong email and/or password'
                 })
             }
+            if(!user.verified) {
+                res.status(401).json({
+                    error: 'Your account is not verified. Please check your email to confirm your account'
+                })
+            }
             user.password = undefined
             const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
             res.json({
@@ -67,7 +88,41 @@ const loginUser = async (req, res) => {
     }
 }
 
+const verifyUser = async(req, res) => {
+    try {
+        const user = await User.findOne({_id: req.params.id})
+        if(!user) {
+            return res.status(400).json({
+                message: 'Invalid link'
+            })
+        }
+
+        const token = await Token.findOne({
+            userId: user._id,
+            token : req.params.token
+        })
+
+        if(!token) {
+            return res.status(400).json({
+                message: 'Invalid link'
+            })
+        };
+
+        await User.updateOne({_id: user._id}, {verified: true});
+        await token.remove();
+
+        res.status(200).json({
+            message: 'Email verified successfully'
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    verifyUser
 }
